675d248ddbb1efdbf67faaac66512959887a7702
UliG1430 <ulises.geymonat186925@alumnos.info.unlp.edu.ar>
2024-10-13 12:34:46 -0300

    reconfiguro db

--- api/app.py
+++ api/app.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 app = Flask(__name__)
 
 # Configuración de conexión a la base de datos PostgreSQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/api_database'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/AppDSSD'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
