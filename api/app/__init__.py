94caa5b21898869f6fedc53ae39e314de38dae87
UliG1430 <ulises.geymonat186925@alumnos.info.unlp.edu.ar>
2024-11-28 19:29:17 -0300

    instalo CORS api

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from flasgger import Swagger
+from flask_cors import CORS  # Importamos flask-cors
 import os
 
 # Initializing extensions
@@ -13,18 +14,21 @@ jwt = JWTManager()
 def create_app():
     app = Flask(__name__)
 
-    # Configuration
+    # Configuración
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://postgres:postgres@db:5432/api_database')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY', 'dssd-grupo3')
     app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 3600  # Token expires in 1 hour
 
-    # Initialize extensions
+    # Habilitar CORS para permitir solicitudes desde tu frontend
+    CORS(app, origins="http://localhost:5173")  # Cambia por la URL de tu frontend
+
+    # Inicializamos las extensiones
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
 
-    # Initialize Flasgger (Swagger UI)
+    # Inicializar Flasgger (Swagger UI)
     Swagger(app)
 
     @jwt.expired_token_loader
@@ -35,7 +39,7 @@ def create_app():
     def invalid_token_callback(error):
         return jsonify({"msg": "Invalid token"}), 401
 
-    # Register Blueprints
+    # Registrar los Blueprints
     from app.auth.routes import auth_bp
     from app.routes.protected import protected_bp
 
