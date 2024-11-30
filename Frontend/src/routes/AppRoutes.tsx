794d0c5299f4c78f646c78c623049be33191f73c
teozaratiegui <teo.zaratiegui186311@alumnos.info.unlp.edu.ar>
2024-11-30 07:43:02 -0300

    guardo cambios

--- Frontend/src/routes/AppRoutes.tsx
+++ Frontend/src/routes/AppRoutes.tsx
@@ -5,7 +5,7 @@ import Home from '../pages/Home';
 import ProtectedRoute from '../components/ProtectedRoute';
 import Login from '../pages/Login';
 import CargarRecoleccion from '../pages/CargarRecoleccion';
-import StartRecoleccion from '../pages/StartRecoleccion'; // Import the new page
+import StartRecoleccion from '../pages/StartRecoleccion';
 import VisitarPunto from '../pages/VisitarPunto';
 import EsperarCobro from '../pages/EsperarCobro';
 import EntregarPaquete from '../pages/EntregarPaquete';
@@ -13,9 +13,13 @@ import Paquetes from '../pages/Paquetes';
 import AnalizarOrdenes from '../pages/AnalizarOrdenes';
 import RegistrarResultado from '../pages/RegistrarResultado';
 import ProtectedRouteRedGlobal from '../components/ProtectedRouteRedGlobal';
-import HomeRedGlobal from '../pages/HomeRedGlobal'; 
+import HomeRedGlobal from '../pages/HomeRedGlobal';
 import LoginRedGlobal from '../pages/LoginRedGlobal';
 import Necesidades from '../pages/Necesidades';
+import OrdenesDistribucion from '../pages/OrdenesDistribucion'; // Importamos la nueva página
+import EsperarEvaluacion from '../pages/EsperarEvaluacion';
+import Evaluaciones from '../pages/Evaluaciones';
+import RealizarEvaluacion from '../pages/RealizarEvaluacion';
 
 const AppRoutes: React.FC = () => {
   return (
@@ -24,19 +28,23 @@ const AppRoutes: React.FC = () => {
       <Route path="/iniciar-sesion" element={<Login />} />
 
       <Route element={<ProtectedRoute />}>
-        <Route path="/red-global-recicladores" element={<LoginRedGlobal />} /> {/* Ruta para login en red global */}
+        <Route path="/red-global-recicladores" element={<LoginRedGlobal />} />
         <Route path="/cargar-recoleccion" element={<CargarRecoleccion />} />
-        <Route path="/comenzar-recoleccion" element={<StartRecoleccion />} /> {/* Add the new protected route */}
+        <Route path="/comenzar-recoleccion" element={<StartRecoleccion />} />
         <Route path="/visitar-punto" element={<VisitarPunto />} />
         <Route path="/esperar-cobro" element={<EsperarCobro />} />
         <Route path="/entregar-paquetes" element={<EntregarPaquete />} />
         <Route path="/paquetes" element={<Paquetes />} />
         <Route path="/analizar-ordenes/:paqueteId" element={<AnalizarOrdenes />} />
         <Route path="/registrar-resultado/:caseId" element={<RegistrarResultado />} />
+        <Route path="/esperar-evaluacion" element={<EsperarEvaluacion />} />
+        <Route path="/evaluaciones" element={<Evaluaciones />} />
+        <Route path="/realizar-evaluacion" element={<RealizarEvaluacion />} />
       </Route>
       <Route element={<ProtectedRouteRedGlobal />}>
         <Route path="/home-red-global" element={<HomeRedGlobal />} />
-        <Route path="/necesidades" element={<Necesidades/>} />
+        <Route path="/necesidades" element={<Necesidades />} />
+        <Route path="/ordenes-distribucion" element={<OrdenesDistribucion />} />
       </Route>
     </Routes>
   );
